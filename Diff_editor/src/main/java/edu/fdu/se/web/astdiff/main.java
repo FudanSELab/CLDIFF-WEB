e97192da5759318ab7070bce7ad23f9036bc20dd
basti <876572078@qq.com>
2018-05-31 20:39:12 +0800

    对接爬虫

--- Diff_editor/src/main/java/edu/fdu/se/web/astdiff/main.java
+++ Diff_editor/src/main/java/edu/fdu/se/web/astdiff/main.java
@@ -3,6 +3,7 @@ package edu.fdu.se.web.astdiff;
 public class main {
 
     public static void main(String[] args){
-        String result = HttpClient.doPost("http://127.0.0.1:8081/fetchFile&a=b", "c=d&b=e");
+        String result = HttpClient.doGet("https://github.com/spring-projects/spring-framework/commit/3c1adf7f6af0dff9bda74f40dabe8cf428a62003");
+        System.out.println(result);
     }
 }

fb17d048f41f465c8c61395a3ed870f8c15604f8
calvinhkf <cavinkfh@gmail.com>
2018-10-10 14:53:19 +0800

    add config.properties

--- src/main/java/edu/fdu/se/web/astdiff/API.java
+++ src/main/java/edu/fdu/se/web/astdiff/API.java
@@ -2,10 +2,21 @@ package edu.fdu.se.web.astdiff;
 
 public class API {
 
-    public static final String SERVER = "http://10.141.221.83:8081";
-//    public static final String SERVER = "http://localhost:8081";
+    public static final String SERVER_REMOTE = "http://10.141.221.83:8081";
+    public static final String SERVER_LOCAL = "http://localhost:8081";
 
-    public static final String FETCH_META = SERVER;
-    public static final String FETCH_CONTENT = SERVER+"/fetchFile";
+    {
+        String value = ProjectProperties.getInstance().getValue("deploy_type");
+
+        if("remote".equals(value)){
+            SERVER = SERVER_REMOTE;
+        }else{
+            SERVER = SERVER_LOCAL;
+        }
+    }
+    public static String SERVER;
+
+    public static String FETCH_META = SERVER;
+    public static String FETCH_CONTENT = SERVER+"/fetchFile";
 
 }
--- src/main/java/edu/fdu/se/web/astdiff/main.java
+++
@@ -1,9 +0,0 @@
-package edu.fdu.se.web.astdiff;
-
-public class main {
-
-    public static void main(String[] args){
-        String result = HttpClient.doGet("https://github.com/spring-projects/spring-framework/commit/3c1adf7f6af0dff9bda74f40dabe8cf428a62003");
-        System.out.println(result);
-    }
-}

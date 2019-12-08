6fd4274131d1f066c53e1f722643141d584fa943
huangkaifeng <calvinkfh@gmail.com>
2019-12-08 19:16:53 +0800

    migration to aliyun

--- src/main/java/edu/fdu/se/web/astdiff/API.java
+++ src/main/java/edu/fdu/se/web/astdiff/API.java
@@ -7,7 +7,7 @@ public class API {
      * deploy_type = online
      * repo_source = online
      */
-    public static final String ONLINE_SERVER_REMOTE = "http://10.141.221.83:8081";
+    public static final String ONLINE_SERVER_REMOTE = "http://106.14.239.166:8081";
     /**
      * Debugging CodeWisdom local
      * deploy_type = online
